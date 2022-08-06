b18c7d6927b85aa9d5c7ba4b253cb546343417fa
z1n0v1 <z1n0v1@students.softuni.bg>
2022-08-06 17:46:15 +0300

    Translated Capability Descriptions to Bulgarian

--- src/main/java/eu/zinovi/receipts/domain/model/enums/CapabilityEnum.java
+++ src/main/java/eu/zinovi/receipts/domain/model/enums/CapabilityEnum.java
@@ -1,45 +1,45 @@
 package eu.zinovi.receipts.domain.model.enums;
 
 public enum CapabilityEnum {
-    CAP_ADMIN( "Admin privilege"),
-    CAP_ADMIN_LIST_RECEIPTS( "Admin list receipts"),
-    CAP_ADMIN_VIEW_RECEIPT( "Admin view receipt"),
+    CAP_ADMIN( "Админ права"),
+    CAP_ADMIN_LIST_RECEIPTS( "Админ списък бележки"),
+    CAP_ADMIN_VIEW_RECEIPT( "Админ разглеждане бележка"),
 
-    CAP_ADD_USER("Add user"),
-    CAP_VIEW_USER_DETAILS("View user details"),
-    CAP_LIST_USERS("List users"),
-    CAP_EDIT_USER("Edit user"),
-    CAP_EDIT_ALL_USERS("Edit all users"),
-    CAP_DELETE_USER("Delete user"),
+    CAP_ADD_USER("Добавяне потребител"),
+    CAP_VIEW_USER_DETAILS("Разглеждане потребителски детайли"),
+    CAP_LIST_USERS("Списък потребители"),
+    CAP_EDIT_USER("Редакция потребител"),
+    CAP_EDIT_ALL_USERS("Редакция всички потребители"),
+    CAP_DELETE_USER("Изтриване потребител"),
 
-    CAP_ADD_ROLE("Add role"),
-    CAP_LIST_ROLES("List roles"),
-    CAP_EDIT_ROLE("Edit role"),
-    CAP_DELETE_ROLE("Delete role"),
+    CAP_ADD_ROLE("Добавяне роля"),
+    CAP_LIST_ROLES("Списък роли"),
+    CAP_EDIT_ROLE("Редакция роля"),
+    CAP_DELETE_ROLE("Изтриване роля"),
 
-    CAP_LIST_CAPABILITIES("List capabilities"),
-    CAP_EDIT_CAPABILITY("Edit capability"),
+    CAP_LIST_CAPABILITIES("Списък права"),
+    CAP_EDIT_CAPABILITY("Редакция права"),
 
-    CAP_LIST_CATEGORIES("List categories"),
-    CAP_ADD_CATEGORY("Add category"),
-    CAP_EDIT_CATEGORY("Edit category"),
-    CAP_DELETE_CATEGORY("Delete category"),
+    CAP_LIST_CATEGORIES("Списък категории"),
+    CAP_ADD_CATEGORY("Добавяне категория"),
+    CAP_EDIT_CATEGORY("Редакция категория"),
+    CAP_DELETE_CATEGORY("Изтриване категория"),
 
-    CAP_VIEW_HOME("View home"),
+    CAP_VIEW_HOME("Разглеждане на главната страница"),
 
-    CAP_ADD_RECEIPT("Add receipt"),
-    CAP_VIEW_RECEIPT("View receipt"),
-    CAP_EDIT_RECEIPT("Edit receipt"),
-    CAP_LIST_RECEIPTS("List receipts"),
-    CAP_LIST_ALL_RECEIPTS("List all receipts"),
-    CAP_DELETE_RECEIPT("Delete receipt"),
+    CAP_ADD_RECEIPT("Добавяне бележка"),
+    CAP_VIEW_RECEIPT("Разглеждане бележка"),
+    CAP_EDIT_RECEIPT("Редакция бележка"),
+    CAP_LIST_RECEIPTS("Списък бележки"),
+    CAP_LIST_ALL_RECEIPTS("Списък всички бележки"),
+    CAP_DELETE_RECEIPT("Изтрриване бележка"),
 
-    CAP_DELETE_ITEM("Delete item"),
-    CAP_ADD_ITEM("Add item"),
-    CAP_EDIT_ITEM("Edit item"),
-    CAP_LIST_ITEMS("List items"),
+    CAP_DELETE_ITEM("Изтриване артикул"),
+    CAP_ADD_ITEM("Добавяне артикул"),
+    CAP_EDIT_ITEM("Редакция артикул"),
+    CAP_LIST_ITEMS("Списък артикули"),
 
-    CAP_CHANGE_PASSWORD("Change password");
+    CAP_CHANGE_PASSWORD("Промяна парола");
 
 
     private final String description;
